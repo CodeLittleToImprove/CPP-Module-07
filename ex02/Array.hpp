b5f874cb350aff3984af2aa9c00597544c9691ce
tbui-quo <tbui-quo@student.42wolfsburg.de>
2025-09-16 17:45:13 +0200

    work on main in ex-2

---
+++ ex02/Array.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+# include <iostream>
+# include <exception>
+
+template <typename T>
+class Array
+{
+	private:
+		T *_data;
+		unsigned int _size;
+
+	public:
+		Array();
+		Array(unsigned int n);
+		Array(const Array &other);
+		Array& operator=(const Array &other);
+		~Array();
+
+		T &operator[](unsigned int index);
+		T const &operator[](unsigned int index) const;
+		unsigned int size() const;
+
+		class OutOfBoundsException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+// When you define a template class, the compiler needs to see both:
+// The declaration and
+// the implementation of all member functions
+
+
+};
+
+# include "Array.tpp"
